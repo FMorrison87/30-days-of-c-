05d4875db642612df1a007a68c773e3103f9fa65
Frances Morrison <francesmorrison@Francess-MacBook-Pro.local>
2024-10-23 07:23:30 -0400

    task: class and encapsulation project.

--- classes-and-encapsulation/Program.cs
+++ classes-and-encapsulation/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Book book = new Book();
+        book.Title = "The Furious Fortitude";
+        book.Author = "Dangus Dangle";
+        book.Price = 16.50;
+        book.DisplayBookInfo();
+
+    }
+
+    class Book
+    {
+        public string Title { get; set; } = string.Empty;
+        public string Author { get; set; } = string.Empty;
+        private double price = 0;
+        public double Price
+        {
+            get { return price; }
+            set
+            {
+                if (price < 0)
+                {
+                    Console.WriteLine("Price must be greater than zero.");
+                }
+                else
+                {
+                    price = value;
+                }
+            }
+        }
+
+        public void DisplayBookInfo() {
+            Console.WriteLine($"Title: {Title}, Author: {Author}, Price: ${Price}");
+        }
+    }
+}
